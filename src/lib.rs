f87111b303866ff556bf953927422166466ca0ec
Bennet Bleßmann <bb-github@t-online.de>
2024-08-25 21:32:22 +0200

    expand the crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,24 @@
 #![deny(clippy::undocumented_unsafe_blocks)]
 #![warn(missing_docs)]
 
-//! Arc based Rcu implementation based on my implementation in [mthom/scryer-prolog#1980](https://github.com/mthom/scryer-prolog/pull/1980)
+//! Arc based Rcu implementation originally implementated in [mthom/scryer-prolog#1980](https://github.com/mthom/scryer-prolog/pull/1980)
 //!
 //! ```text
-//! Arc
-//!  Rcu
-//! Arcu
+//! A r c
+//!   R c u
+//! A r c u
 //! ```
-
+//!
+//! The atomics based version performs lock-free[^1] reads.
+//! By using Arc we keep the **r**ead-**c**ritical-**s**ection short and free of user defined code and
+//! automatically perform cleanup when no reference remains.
+//!
+//! To coordinate reads and writes [EpochCounter]s from an [EpochCounterPool] are used.
+//! Each read used an `EpochCounter` from the `EpochCounterPool` of the `Arcu` incrementing it once before entering the RCS and once more on leaving the RCS.
+//! Each write checks against all `EpochCounter`s in the pool, blocking until it is safe to decrement the strong count of the `Arc` that was replaced by the write.
+//!
+//! [^1]: when using thread local epoch counter with the global epoch counter pool, the initial read may block while adding the threads epoch counter to the pool
+//!
 extern crate alloc;
 
 pub mod epoch_counters;
